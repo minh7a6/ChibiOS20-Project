df87042ed13fc24913088b688b775e467f4b605c
minh7a6 <minhdangle7a6@gmail.com>
2021-08-20 16:10:05 -0600

    added blinky sample project with cmake

---
+++ blinky/boardCfg/board.mk
@@ -0,0 +1,9 @@
+# List of all the board related files.
+BOARDSRC = boardCfg/board.c
+
+# Required include directories
+BOARDINC = boardCfg
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
